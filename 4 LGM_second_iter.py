ef2b7a7b7207d9423f25f4cf3fa3af5b61a74874
Abhinav Sharma <abhinavs01858@gmail.com>
2020-04-13 18:36:20 +0530

    Update 4 LGM_second_iter.py

--- 4 LGM_second_iter.py
+++ 4 LGM_second_iter.py
@@ -3,6 +3,7 @@ import gc
 import numpy as np, pandas as pd
 import lightgbm as lgb
 
+
 CAL_DTYPES={"event_name_1": "category", "event_name_2": "category", "event_type_1": "category", 
          "event_type_2": "category", "weekday": "category", 'wm_yr_wk': 'int16', "wday": "int16",
         "month": "int16", "year": "int16", "snap_CA": "float32", 'snap_TX': 'float32', 'snap_WI': 'float32' }
